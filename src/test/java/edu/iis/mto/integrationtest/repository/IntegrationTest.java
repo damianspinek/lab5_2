cf5db0eea0ab825c2ba4444ce00ada560c6757ef
Damian Spinek <damian00737@gmail.com>
2015-04-27 11:58:02 +0200

    Naprawienie testu poprzez usuniecie dodanego przez test rekordu po zakonczeniu testu

--- src/test/java/edu/iis/mto/integrationtest/repository/IntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/IntegrationTest.java
@@ -1,12 +1,19 @@
 package edu.iis.mto.integrationtest.repository;
 
 import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import edu.iis.mto.integrationtest.config.ApplicationConfig;
 import edu.iis.mto.integrationtest.utils.Mode;
 import edu.iis.mto.integrationtest.utils.ModeUtils;
 
+@ContextConfiguration(classes = { ApplicationConfig.class })
+// kontekst Spring
+@RunWith(SpringJUnit4ClassRunner.class)
+// uruchamianie z wykorzystaniem specyficznego wykonawcy
 public class IntegrationTest {
-
 	@BeforeClass
 	public static void setUpMode() {
 		ModeUtils.setMode(Mode.TEST);
